2f172184a41b49cd38d121d115a02b5c7ecf387f
Purvi <purvithakkar358@gmail.com>
2023-01-27 12:30:56 +0000

    ages updated

--- day2_python_basics/day2_ages.py
+++ day2_python_basics/day2_ages.py
@@ -36,35 +36,35 @@ ages =[12,18,33,84,45,67,12,82,95,16,10,23,43,29,40,34,30,16,44,69,70,74,38,65,3
 
 age_16_to_65 = []
 
-for i in range(len(ages)):
-    if i >= 16 or i > 65:
+for i in ages:
+    if i >= 16 and i < 65:
         age_16_to_65.append(i)
-    if i >= 65:
-        break
 print(age_16_to_65)
 
 
 
 # Display the count of 16 – 25 year olds in the new list
 
+
 ages =[12,18,33,84,45,67,12,82,95,16,10,23,43,29,40,34,30,16,44,69,70,74,38,65,36,83,50,11,79,64,78,37,3,8,68,22,4,60,33,82,45,
         23,5,18,28,99,17,81,14,88,50,19,59,7,44,93,35,72,25,63,11,69,11,76,10,60,30,14,21,82,47,6,21,88,46,78,92,48,36,28,51]
+
 age_16_to_25 = []
 
-for i in range(len(ages)):
-    if i >= 16 or i > 25:
+for i in ages:
+    if i >= 16 and i < 25:
         age_16_to_25.append(i)
-    if i >= 25:
-        break
 print(age_16_to_25)
 print(len(age_16_to_25))
 
 # Sort the ages of the new list (hint use <list>.sort())
 
-
-
+age_16_to_25.sort()
+print(age_16_to_25)
 
 # What proportion of people belong in the 16 – 25 category within the new list """
+
+
 
 
 
